09349667601e4c8471445f1e4f28e0e8f3af6b9b
nihihi <nihihi@gmail.com>
2024-08-15 17:15:56 +0200

    cleanup

--- src/main/java/nh/weather_app_kafka/config/AppConfig.java
+++ src/main/java/nh/weather_app_kafka/config/AppConfig.java
@@ -1,20 +1,9 @@
 package nh.weather_app_kafka.config;
 
-import nh.weather_app_kafka.model.CurrentWeather;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.springframework.web.client.RestTemplate;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class AppConfig {
--- src/main/java/nh/weather_app_kafka/model/CurrentWeather.java
+++ src/main/java/nh/weather_app_kafka/model/CurrentWeather.java
@@ -6,50 +6,80 @@ import java.time.LocalDateTime;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CurrentWeather {
-   private String time;
-   private double temperature;
-   private double windspeed;
-   private int winddirection;
+    private String time;
+    private int interval;
+    private double temperature;
+    private double windspeed;
+    private int winddirection;
+    private int is_day;
+    private int weathercode;
 
-   public String getTime() {
-      return time;
-   }
+    public String getTime() {
+        return time;
+    }
 
-   public void setTime(String time) {
-      this.time = time;
-   }
+    public void setTime(String time) {
+        this.time = time;
+    }
 
-   public double getTemperature() {
-      return temperature;
-   }
+    public double getTemperature() {
+        return temperature;
+    }
 
-   public void setTemperature(double temperature) {
-      this.temperature = temperature;
-   }
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
 
-   public double getWindspeed() {
-      return windspeed;
-   }
+    public double getWindspeed() {
+        return windspeed;
+    }
 
-   public void setWindspeed(double windspeed) {
-      this.windspeed = windspeed;
-   }
+    public void setWindspeed(double windspeed) {
+        this.windspeed = windspeed;
+    }
 
-   public int getWinddirection() {
-      return winddirection;
-   }
+    public int getWinddirection() {
+        return winddirection;
+    }
 
-   public void setWinddirection(int winddirection) {
-      this.winddirection = winddirection;
-   }
+    public void setWinddirection(int winddirection) {
+        this.winddirection = winddirection;
+    }
 
-   @Override
-   public String toString() {
-      return "CurrentWeather{" +
-              "time='" + time + '\'' +
-              ", temperature=" + temperature +
-              ", windspeed=" + windspeed +
-              ", winddirection=" + winddirection +
-              '}';
-   }
+    public int getInterval() {
+        return interval;
+    }
+
+    public void setInterval(int interval) {
+        this.interval = interval;
+    }
+
+    public int getIs_day() {
+        return is_day;
+    }
+
+    public void setIs_day(int is_day) {
+        this.is_day = is_day;
+    }
+
+    public int getWeathercode() {
+        return weathercode;
+    }
+
+    public void setWeathercode(int weathercode) {
+        this.weathercode = weathercode;
+    }
+
+    @Override
+    public String toString() {
+        return "CurrentWeather{" +
+                "time='" + time + '\'' +
+                ", interval=" + interval +
+                ", temperature=" + temperature +
+                ", windspeed=" + windspeed +
+                ", winddirection=" + winddirection +
+                ", is_day=" + is_day +
+                ", weathercode=" + weathercode +
+                '}';
+    }
 }
